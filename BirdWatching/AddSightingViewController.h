1667dbbcc82fc1ae1349bfb1adf1fb72e224705a
CURTIS STOCHL <cdstochl@SH308-25.local>
2013-01-24 12:22:11 -0600

    I finished a bunch of stuff.  I finished designing the add page, and I just need to link it to the thing and other stuff.

---
+++ BirdWatching/AddSightingViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddSightingViewController.h
+//  BirdWatching
+//
+//  Created by CURTIS STOCHL on 1/24/13.
+//  Copyright (c) 2013 CURTIS STOCHL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddSightingViewController : UITableViewController
+
+@end
